5d61f84cd2ef303be5b5ca83af210e40b9dc12ab
rdsalangsang <rosa.salangsang@sjsu.edu>
2021-12-09 14:04:51 -0800

    final?

--- sketch.js
+++ sketch.js
@@ -7,7 +7,11 @@ let heroPosX=300;
 let heroPosY=300;
 let boss;
 let BobHealth = 0;
+let BulletHell=[];
 let shots=0;
+let radius=0;
+let xpos=0;
+let ypos=370;
 
 
 function setup() {
@@ -15,6 +19,9 @@ function setup() {
   textFont('monospace');
   hero = new Player(300,300);
   boss = new Bob();
+  for (let i=0;i<50;i++){
+    BulletHell[i]= new Projectile(this.x,this.y,this.r)
+  }
 }
 
 function draw() {
@@ -35,6 +42,10 @@ function draw() {
       victory();
       cnv.mouseClicked(victoryMouseClicked);
       break;
+      case 'lose':
+        lose();
+        cnv.mouseClicked(loseMouseClicked);
+        break;
     default:
       break;
   }
@@ -65,22 +76,63 @@ function title() {
 function intro(){
   background(50);
   textSize(15);
+  noStroke();
   text("Bob tires of Rosa's shenangigans and has taken over the game", 300, 100);
   text("that they were working on for a class project.", 300, 130);
   text("Defeat Bob so that Rosa can finish their project.", 300, 160);
   text("Use WASD to move", 300, 290);
-  text("Click to shoot", 300, 320);
+  text("Get Bob's health to zero", 300, 320);
 }
 
 function level() {
   background(50);
-  hero.show();
+  hero.display();
   hero.move();
+  BulletHell[i].display();
+  BulletHell[i].fire();
   boss.display();
   fill(52, 235, 70);
-  rect(10,550,250,10);
-  stroke(50);
+  rect(10,550,(300-BobHealth),10);
+  stroke(250);
   boss.health();
+  //player bullets
+  let radius = 2.5;
+  if(ypos>150){
+    ypos=ypos-10;
+  }
+  else{
+    ypos=hero.y-hero.r;
+    xpos=hero.x;
+    BobHealth=BobHealth+2;
+  }
+  stroke(240);
+  strokeWeight(1);
+  fill(0);
+  ellipse(heroPosX,heroPosY,radius);
+///hero BobHealth
+noStroke
+fill(52,235,70);
+stroke(250);
+rect(580-((5-shots)*30)-2.5,550,160,10);
+fill(250);
+noStroke();
+text("You",575,580);
+
+//oh i've been hit
+var d=dist(heroPosX,heroPosY,BulletHell[i].x,BulletHell[i].y);
+if(d<(hero.r+BulletHell[i].r)){
+  shots=shots+1
+  hero.shot();
+  BulletHell[i].shot();
+}
+//loser
+if (shots>3){
+  state='lose';
+}
+//Winner
+if (BobHealth>299){
+  state='victory';
+}
 }
 
 
@@ -92,36 +144,45 @@ function titleMouseClicked() {
 function introMouseClicked() {
   state = 'level';
 }
-// function level1MouseClicked() {
-//   points++;
-//   if (points >= 20) {
-//     state = 'victory'
-//   }
-// }
 
 function victory() {
-  background(59, 59, 245);
+  background(50);
   textSize(75);
   stroke(255);
   textAlign(CENTER);
   text('Winner!', 300, 100);
   textSize(25);
-  text('click to restart', 300, 300)
+  text('refresh to play again', 300, 300)
 }
 
-function victoryMouseClicked() {
-  state = 'title'
-  points = 0;
+function lose(){
+  background(50);
+  textSize(75);
+  stroke(250);
+  textAlign(CENTER);
+  text("You lose!",300,100);
+  textSize(25);
+  text('refresh to try again',300,300)
 }
+// function victoryMouseClicked() {
+//   state = 'title';
+// }
+//
+// function loseMouseClicked(){
+//   state='level'
+// }
 
 class Player{
   constructor(){
+    this.x=600*0.5;
+    this.y=600*0.75;
+    this.r=50;
   }
-  show(){
+  display(){
     stroke(0);
     beginShape();
     fill(183, 15, 245);
-    ellipse(heroPosX,heroPosY,35);
+    ellipse(heroPosX,heroPosY,this.r);
     endShape();
   }
   move() {
@@ -143,11 +204,11 @@ class Player{
     }
   }
   shot(){
-    this.x=this.x*0.5;
-    this.y=this.y*0.75;
+    this.x=heroPosX*0.5;
+    this.y=heroPosY*0.75;
   }
 }
-//test
+
 
 class Bob{
   constructor(){
@@ -157,6 +218,10 @@ class Bob{
     this.h=300;
   }
   display(){
+  noStroke();
+  fill(250);
+  text("man, I just wanna sleep",100,100);
+  text("take some raining bullets",500,100);
   stroke(255);
   strokeWeight(2);
   fill(240);
@@ -171,9 +236,35 @@ class Bob{
   triangle(300, 175, 210, 150, 250, 190);
   ellipse(300, 175, 20);
   }
+
   health(){
     fill(250);
-    noStroke();
+    noStroke()
     text("Bob", 20,580);
+  }
+}
+//test
+class Projectile{
+  constructor(){
+    this.x=random(30,540);
+    this.y=50;
+    this.r=30;
+  }
+  display(){
+    fill(250);
+    ellipse(this.x,this.y,this.r);
+  }
+  fire(){
+    if(this.y<450){
+      this.y=this.y+7
+    }
+    else{
+      this.y=200
+      this.x=random(200,500);
+    }
+  }
+  shot(){
+    this.y=200
+    this.x=random(200,500);
   }
 }
